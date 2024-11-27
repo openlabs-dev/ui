58d02656f1745ed1a710a89086c32ee33ec0d502
Sebastián García <sebastiandotdev@gmail.com>
2024-11-26 21:02:54 -0500

    Remove banner beta in layout.tsx

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -5,9 +5,6 @@ import { docsOptions } from '../layout.config'
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <aside className="sticky py-2 bg-warn text-warn-foreground text-center">
-        <h4 className="font-bold">Documentation is currently in beta</h4>
-      </aside>
       <DocsLayout {...docsOptions}>{children}</DocsLayout>
     </>
   )
